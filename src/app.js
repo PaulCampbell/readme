e88187713cd2781989c78bd6e106bf364800ce16
vagrant <vagrant@precise64.(none)>
2014-01-18 17:56:58 +0000

    a view

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var ncr = require('nodecr');
-var PORT = 8080;
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs'),
+    ncr = require('nodecr');
+
+var PORT = 49001;
 
 var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  res.send('readme');
 });
 
-
 app.get('/ocr', function (req, res) {
-  ncr.process(__dirname + '/test/testfiles/abbey_road.jpg',function(err, text){
-    if(err) return console.error(err)
+  res.render('ocr');
+});
 
-    console.log(text)
-    res.send(text);
-  }, 'eng', 6)
+app.post('/ocr', function (req, res) {
+  fs.readFile(req.files.displayImage.path, function (err, data) {
+    var newPath = __dirname + "/uploads/" + new Date().getTime();
+    fs.writeFile(newPath, data, function (err) {
+      ncr.process(__dirname + newPath, function(err, text){
+        if(err) return console.error(err)
+
+        res.send(text);
+      }, 'eng', 6)
+    });
+  });
 });
 
 app.listen(PORT);
--- src/test/api_spec.coffee
+++ src/test/api_spec.coffee
@@ -1,4 +1,10 @@
 should = require 'should'
 request = require 'request'
 
-request = request.defaults({followRedirect: false})
+
+describe 'finding text', ->
+  it 'returns 201 with the link', (done) ->
+    request("http://localhost:8080").post("/ocr").attach("file", "testfiles/abbey_road.jpg").end (err, res) ->
+      res.should.have.status 200 # 'success' status
+      done()
+
